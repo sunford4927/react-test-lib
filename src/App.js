3611f4fefa46adeb4ee46c03897815ebacab10eb
sunford4927 <p01097220945@gamil.com>
2022-10-17 12:44:54 +0900

    button test add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Profile from './Profile';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       <Profile username= "lsh1012" name= "이상현" />
+      <Counter />
       learn react
     </div>
   );
